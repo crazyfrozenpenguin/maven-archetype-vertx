7f0eb36112e478f765d9b1c1725c859e18c1cba3
ramki <ramki.s@gmail.com>
2013-02-21 22:44:19 +0530

    Adding test deps. Added templating for InContainerTest.java.

--- src/main/resources/archetype-resources/src/test/java/integration/java/InContainerTest.java
+++ src/main/resources/archetype-resources/src/test/java/integration/java/InContainerTest.java
@@ -38,7 +38,7 @@ public class InContainerTest extends TestVerticle {
 
   @Test
   public void testDeployMod() {
-    container.deployModule("maven:com.mycompany:my-module:${version}-SNAPSHOT", new Handler<String>() {
+    container.deployModule("maven:${groupId}:${artifactId}:${version}-SNAPSHOT", new Handler<String>() {
       @Override
       public void handle(String deploymentID) {
         assertNotNull("deploymentID should not be null", deploymentID);
