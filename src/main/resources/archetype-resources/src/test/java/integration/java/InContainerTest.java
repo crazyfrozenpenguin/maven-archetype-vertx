edcd025649c3b7e9967d3f48ec07f8dafdf340ae
ramki <ramki.s@gmail.com>
2013-03-10 14:11:33 +0530

    Made use of system var for the module name.

--- src/main/resources/archetype-resources/src/test/java/integration/java/InContainerTest.java
+++ src/main/resources/archetype-resources/src/test/java/integration/java/InContainerTest.java
@@ -38,7 +38,7 @@ public class InContainerTest extends TestVerticle {
 
   @Test
   public void testDeployMod() {
-    container.deployModule("${groupId}~${artifactId}~${version}", new Handler<String>() {
+    container.deployModule(System.getProperty("vertx.modulename"), new Handler<String>() {
       @Override
       public void handle(String deploymentID) {
         assertNotNull("deploymentID should not be null", deploymentID);
